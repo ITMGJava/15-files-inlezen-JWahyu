28e0745b70f651b7bd668e748ade609ce7051bd9
JWahyu <71404267+JWahyu@users.noreply.github.com>
2020-10-29 11:59:14 +0100

    Test

--- src/FilesInlezen.java
+++ src/FilesInlezen.java
@@ -3,26 +3,22 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class FilesInlezen {
-//    boolean check = false;
 
     public void bestandCheck() {
-//        while (!check) {
             try {
                 bestandThrow();
             } catch (FileNotFoundException f) {
                 System.out.println("Je bestand is niet gevonden. Probeer opnieuw.");
-//                check = false;
+
             }
-//        }
     }
 
     public void bestandThrow() throws FileNotFoundException {
-        File bestandVinden = new File("Hello.txt");
-        Scanner bestandLezen = new Scanner("Hello.txt");
-        if (bestandLezen.hasNextLine()) {
+        File bestandVinden = new File("Hello2.txt");
+        Scanner bestandLezen = new Scanner(bestandVinden);
+        while (bestandLezen.hasNextLine()) {
             System.out.println(bestandLezen.nextLine());
         }
-//        check = true;
     }
 
     static void bestandInlezen(String bestandsNaam) {
