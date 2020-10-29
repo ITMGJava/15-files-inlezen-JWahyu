5694b617553a29c0389c4f463c50c2699285d3ca
JWahyu <71404267+JWahyu@users.noreply.github.com>
2020-10-29 11:54:12 +0100

    Test

--- src/FilesInlezen.java
+++ src/FilesInlezen.java
@@ -1,3 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FilesInlezen {
+    boolean check = false;
+
+    public void bestandCheck() {
+        while (!check) {
+            try {
+                bestandThrow();
+            } catch (FileNotFoundException f) {
+                System.out.println("Je bestand is niet gevonden. Probeer opnieuw.");
+                check = false;
+            }
+        }
+    }
+
+    public void bestandThrow() throws FileNotFoundException {
+        File bestandVinden = new File("Hello.txt");
+        Scanner bestandLezen = new Scanner("Hello.txt");
+        if (bestandLezen.hasNextLine()) {
+            System.out.println(bestandLezen);
+        }
+        check = true;
+    }
+
+    static void bestandInlezen(String bestandsNaam) {
+
+    }
+
+
+
+    public static void main(String[] args) {
+// Leeg
+    }
+
 }
--- src/FilesInlezen2.java
+++ src/FilesInlezen2.java
@@ -1,4 +1,8 @@
-
 public class FilesInlezen2 {
+
+    public static void main(String[] args) {
+        FilesInlezen bestand = new FilesInlezen();
+        bestand.bestandCheck();
+    }
 
 }
